418aa7ab2a527a17cab7f00b7db140675967fd5f
veroena <veronica.enamorado@gmail.com>
2019-06-12 18:28:27 +0200

    Fix linter errors and delete console.log

--- js/main.js
+++ js/main.js
@@ -10,29 +10,27 @@ function getRandomNumber(min, max) {
 }
 
 const alterEgo = () => {
-  
-  const nameNumber = getRandomNumber(1, 213)
+  const nameNumber = getRandomNumber(1, 213);
   fetch(`https://www.anapioficeandfire.com/api/characters?page=${nameNumber}`)
-  .then(response => response.json())
-  .then(data => {
-    const number = getRandomNumber(0, 10);
-    const splitName = data[number].name.split(' ');
-    results1.innerHTML = `Your name is ${splitName[0]}, `;
-  });
-  const houseNumber = getRandomNumber(8)
+    .then(response => response.json())
+    .then(data => {
+      const number = getRandomNumber(0, 10);
+      const splitName = data[number].name.split(' ');
+      results1.innerHTML = `Your name is ${splitName[0]}, `;
+    });
+  const houseNumber = getRandomNumber(8);
   fetch(`https://www.anapioficeandfire.com/api/houses?pagesize=50&page=${houseNumber}`)
-  .then(response => response.json())
-  .then(data => {
-    const number = getRandomNumber(0, 50);
-    const number2 = getRandomNumber(0, 50);
-    results2.innerHTML = `you belong in ${data[number].name},`
-    results3.innerHTML = `and you were born in ${data[number2].region}.`
-  })
-}
+    .then(response => response.json())
+    .then(data => {
+      const number = getRandomNumber(0, 50);
+      const number2 = getRandomNumber(0, 50);
+      results2.innerHTML = `you belong in ${data[number].name},`;
+      results3.innerHTML = `and you were born in ${data[number2].region}.`;
+    });
+};
 
 const deadOrAlive = () => {
   const number = getRandomNumber(0, 100);
-  console.log(number);
   if (number % 3 === 0) {
     results1.innerHTML = `You're dead 💀`;
     results2.innerHTML = `I'm sorry`;
